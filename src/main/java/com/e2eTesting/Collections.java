61ff596b95a8ef02b4eb3457a7daa1ef5a748d39
talareq <galuszka.grzegorz@yahoo.com>
2019-11-05 10:49:52 +0100

    Collections

---
+++ src/main/java/com/e2eTesting/Collections.java
@@ -0,0 +1,24 @@
+package com.e2eTesting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by m on 2019-11-05.
+ */
+public class Collections {
+  public static void main(String[] args) {
+    String[] langs = {"java", "C#"};
+
+    for (String l : langs) {
+      System.out.println("I would like to learn lang " + l + ".");
+    }
+
+    List<String> languages = Arrays.asList( "Python", "PHP");
+
+    for (String l : languages) {
+      System.out.println("I would like to learn lang " + l + ".");
+    }
+  }
+}
