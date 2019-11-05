2d7dd434337a82437f52660aba9f8a9abf214a20
talareq <galuszka.grzegorz@yahoo.com>
2019-11-05 11:44:53 +0100

    Using of collection in Group Test

--- addressbook-web-tests/src/test/java/com/ete/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/ete/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package com.ete.addressbook.tests;
 
 import com.ete.addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -8,9 +9,11 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before + 1);
   }
 
 }
