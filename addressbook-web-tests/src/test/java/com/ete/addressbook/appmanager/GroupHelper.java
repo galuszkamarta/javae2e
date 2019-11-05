450ddf01bd92e4711ffc91455223bb1d11d5559a
talareq <galuszka.grzegorz@yahoo.com>
2019-11-05 12:56:00 +0100

    Comparing the size of the collection

--- addressbook-web-tests/src/test/java/com/ete/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/com/ete/addressbook/appmanager/GroupHelper.java
@@ -4,7 +4,11 @@ import com.ete.addressbook.model.ContactData;
 import com.ete.addressbook.model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by m on 2019-10-22.
@@ -65,6 +69,18 @@ public class GroupHelper extends HelperBase {
 
   public int getGroupCount() {
     return driver.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
+    for (WebElement element: elements) {
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+    return groups;
+
   }
 }
 
