6100331d29e330f1adc10145badd8dae743f8bda
talareq <galuszka.grzegorz@yahoo.com>
2019-12-04 11:20:13 +0100

    Loading test data from a file in GroupCreationTest

--- addressbook-web-tests/src/test/java/com/ete/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/ete/addressbook/tests/GroupCreationTests.java
@@ -7,6 +7,8 @@ import org.testng.annotations.Test;
 import com.ete.addressbook.model.GroupData;
 import com.ete.addressbook.model.Groups;
 
+import java.io.*;
+import java.security.acl.Group;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,16 +19,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() {
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-    list.add(new Object[]{new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-    list.add(new Object[]{new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (!"".equals(line)) {
+      String[] split = line.split(":");
+      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
   @Test (dataProvider = "validGroups")
   public void testGroupCreation(GroupData group) {
+      app.group().groupCache = null;
       app.goTo().groupPage();
       Groups before = app.group().all();
       app.group().create(group);
@@ -34,6 +41,7 @@ public class GroupCreationTests extends TestBase {
       Groups after = app.group().all();
       assertThat(after, equalTo(
               before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
   }
 
   @Test
