579f8ebd0d339f500aa2b82dba97b53283fb3892
talareq <galuszka.grzegorz@yahoo.com>
2019-12-06 10:39:44 +0100

    Test_stand_configuration

--- addressbook-web-tests/src/test/java/com/ete/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/com/ete/addressbook/appmanager/ApplicationManager.java
@@ -5,30 +5,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by m on 2019-10-22.
  */
 public class ApplicationManager {
+  private final Properties properties;
   WebDriver driver;
 
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
   private ContactHelper contactHelper;
 
   private String baseUrl;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
-  private String browser;
 
-  public ApplicationManager(String browser) {
-
+  public ApplicationManager(String browser){
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
     if (browser.equals(BrowserType.FIREFOX)){
       driver = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)) {
@@ -36,23 +46,19 @@ public class ApplicationManager {
     } else if (browser.equals(BrowserType.IE)) {
       driver = new InternetExplorerDriver();
     }
-    baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-
-    driver.get("http://localhost/addressbook/");
+    driver.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(driver);
     contactHelper = new ContactHelper(driver);
     navigationHelper = new NavigationHelper(driver);
     sessionHelper = new SessionHelper(driver);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
 
   public void stop() {
     driver.quit();
   }
-
-
 
   public GroupHelper group() {
     return groupHelper;
